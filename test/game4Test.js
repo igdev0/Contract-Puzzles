52112947843bd00c3cd967ab964c1be89f10c6bd
igdev <dorultanianos@gmail.com>
2025-04-08 11:20:42 +0300

    complete 4th challenge

--- test/game4Test.js
+++ test/game4Test.js
@@ -5,15 +5,16 @@ describe('Game4', function () {
   async function deployContractAndSetVariables() {
     const Game = await ethers.getContractFactory('Game4');
     const game = await Game.deploy();
-
-    return { game };
+    const players = [ethers.provider.getSigner(0), ethers.provider.getSigner(0)];
+    return { game, players };
   }
   it('should be a winner', async function () {
-    const { game } = await loadFixture(deployContractAndSetVariables);
+    const { game, players } = await loadFixture(deployContractAndSetVariables);
 
     // nested mappings are rough :}
+    await game.connect(players[0]).write(players[1].getAddress());
 
-    await game.win();
+    await game.connect(players[1]).win(players[0].getAddress());
 
     // leave this assertion as-is
     assert(await game.isWon(), 'You did not win the game');
